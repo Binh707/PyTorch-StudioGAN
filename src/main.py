ccffba6b824f276fb770e573be9ddbda1e8850ab
Binh707 <hoanhbinhqt@gmail.com>
2024-07-07 11:56:23 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -151,6 +151,8 @@ def load_configs_initialize_training():
                                             data_dir=cfgs.RUN.data_dir,
                                             DATA=cfgs.DATA,
                                             RUN=cfgs.RUN)
+        if 'ASTECH' in cfgs.DATA.name:
+            hdf5_path = cfgs.RUN.data_dir
     else:
         hdf5_path = None
     cfgs.PRE.crop_long_edge, cfgs.PRE.resize_size = crop_long_edge, resize_size
